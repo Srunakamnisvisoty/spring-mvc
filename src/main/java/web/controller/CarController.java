d44274d49423616a54911a775e6cbd487184bb1b
Srunakamnisvisoty <konchic27041998@gmail.com>
2022-07-21 22:30:53 +0400

    2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,22 +1,38 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import web.config.service.CarService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import web.model.Car;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @Autowired
-    private CarService carService;
+    private static final List<Car> carsList = Collections.unmodifiableList(new ArrayList<>() {{
+        carsList.add(new Car(1L, 1, 1));
+        carsList.add(new Car(2L, 2, 2));
+        carsList.add(new Car(3L, 3, 3));
+        carsList.add(new Car(4L, 4, 4));
+        carsList.add(new Car(5L, 5, 5));
+    }});
+
 
     @GetMapping(value = "/cars")
-    public List<Car> getCars(@PathVariable int count) {
-        return carService.getCars(count);
+    public String getAllCars(Model model) {
+        model.addAttribute("carList", carsList);
+        return "cars";
+    }
+
+    @GetMapping(value = "/cars?count={count}")
+    public String getCars(Model model, @PathVariable int count) {
+        model.addAttribute("carList1", count >= carsList.size() ? carsList : carsList.subList(0, count));
+        return "cars";
     }
 }
--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -2,12 +2,12 @@ package web.model;
 
 public class Car {
 
-    private String model;
+    private Long id;
     private int number;
     private int age;
 
-    public Car(String model, int number, int age) {
-        this.model = model;
+    public Car(Long id, int number, int age) {
+        this.id = id;
         this.number = number;
         this.age = age;
     }
